31adc000ad07c7946b14fd797f36fbe6a07feaa9
podiukov.iv <ilia.podiukov@dom.ru>
2025-12-16 17:00:05 +0000

    done

--- base/flask_redis/app.py
+++ base/flask_redis/app.py
@@ -1,9 +1,11 @@
 import time
 import redis
+import os
 from flask import Flask
 
 app = Flask(__name__)
-cache = redis.Redis(host='REDIS_HOST', port=6379)
+redis_host = os.getenv('REDIS_HOST', 'localhost')
+cache = redis.Redis(host=redis_host, port=6379)
 
 def get_hit_count():
     return cache.incr('hits')
